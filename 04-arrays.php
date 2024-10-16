bcfd35c03394955dc9a862ff500e98731235608e
Silas600 <silasrochsmedeiros.senac@gmail.com>
2024-10-16 16:43:58 -0300

    array associativo

--- 04-arrays.php
+++ 04-arrays.php
@@ -21,5 +21,34 @@
     <p>Rock progressivo: <?=$bandas[2]?> </p>
     <p>Ontem estivemos com a turma de <?=$cursos[2]?></p>
 
+    <hr>
+<?php
+// array associativo
+$filme = [
+    "titulo" => "Senhor dos anéis",
+    "autor" => "J.R.R Tolkien",
+    "lancamento" => 2001,
+    "genero" => "fantasia"
+
+];
+?>
+
+<h2> Acessando os dados do array associativo</h2>
+
+<p> O filme <?= $filme["titulo"]?> foi lançado em <?=$filme["lancamento"]?>.</p>
+
+<hr>
+
+<h2>comandos úteis para análise de Arrays</h2>
+
+<h3>print_r (nomeArray)</h3>
+<pre><?=print_r($bandas)?></pre>
+
+<h3>var_dump(nomeArray)</h3>
+<pre><?=var_dump($bandas)?></pre>
+
+
+
+
 </body>
 </html>
