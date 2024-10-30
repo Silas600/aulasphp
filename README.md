7a553791943651c06f10758aef0ac19ceafe55d0
Silas600 <silasrochsmedeiros.senac@gmail.com>
2024-10-30 16:37:52 -0300

    formulário

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aulasphp
  Exemplos de programação de  php
+
+ ## sobre formulários com PHP
+
+ Do ponto de vista do html, nada muda exceto a necessidade de ter atenção ao formulário (get ou post) e á configuração da ação.
+
+ Importante: TODOS os campos do formulário precisam ter o atributo name definido.
+
+ Quanto ao PHP, usaremos alguns comandos chamados de "array superglobal", em especial o $_GET [] e o $_POST[], para acessar os valores preenchidos nos campos do formulários.
---
+++ processamento.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processamento</title>
+</head>
+<body>
+    <h1>Processamento dos dados vindos do formulário HTML</h1>
+
+<?php
+/* capturando os dados atravéis do array $_POST e guardando CADA UM em variável.*/
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$nascimento = $_POST["nascimento"];
+$mensagem = $_POST["mensagem"];
+?>
+
+<h2>Dados recebidos</h2>
+<ul>
+    <li>nome:<?=$nome?></li>
+    <li>E-mail:<?=$email?></li>
+    <li>Data de nascimento:<?=$nascimento?></li>
+    <li>Mensagem:<?=$mensagem?></li>
+</ul>
+
+
+
+</body>
+</html>
